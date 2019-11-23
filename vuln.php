90acebfd80aa3326aa87a28a048990aac920615e
Gabbi Merz <gmerz@palantir.com>
2019-11-22 19:35:58 -0500

    vuln.php

--- vuln.php
+++ vuln.php
@@ -1,3 +1,3 @@
-</html>
-<img src="https://github.com/gcmerz/hackvember/blob/master/rickrollmedaddy.jpg?raw=true"/>
 <html>
+<img src="https://github.com/gcmerz/hackvember/blob/master/rickrollmedaddy.jpg?raw=true"/>
+</html>

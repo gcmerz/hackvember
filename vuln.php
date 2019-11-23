e1842e1de82e2b3d4348fe40124f2cbb9dd2cee6
Gabbi Merz <gmerz@palantir.com>
2019-11-22 19:35:39 -0500

    vuln.php

--- vuln.php
+++ vuln.php
@@ -1,6 +1,3 @@
-<?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-echo "I'm about to learn PHP!<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.";
-?>
+</html>
+<img src="https://github.com/gcmerz/hackvember/blob/master/rickrollmedaddy.jpg?raw=true"/>
+<html>
